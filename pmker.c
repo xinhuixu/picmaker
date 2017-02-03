6feb2653feea777593dd4e8571fc14ca7ba1f31e
xinhuixu <x.xu.1102@gmail.com>
2017-02-03 17:46:14 -0500

    parallel universe

--- pmker.c
+++ pmker.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -7,23 +8,42 @@
 
 int main(){
   int fd, i, j, xres, yres; 
-  xres = 400; yres = 400;
-  fd = open("xd.ppm", O_CREAT|O_RDWR,0777);
+  xres = 256*2; yres = 256*2;
+  fd = open("xd.ppm", O_CREAT | O_RDWR ,0777);
   
-  char h[20]; snprintf(h, sizeof(h), "P3\n%d %d\n255", xres, yres);
-  write(fd, h, sizeof(h));
+  char h[20]; snprintf(h, sizeof(h), "P3\n%d %d\n255\n", xres, yres);
+  write(fd, h, strlen(h) * sizeof(char));
+  
+  /* all black */			
+  /* for (i=0; i<yres; i++){ */
+  /*   for (j=0; j<xres; j++) { */
+  /*     char c[20]; */
+  /*     sprintf(c, "%d %d %d ", 1,1,1); */
+  /*     write(fd, c, strlen(c) * sizeof(char)); */
+  /*   } */
+  /* } */
 
-  char c[4];
+  /* double vertical grayscale gradient */
+  /* for (i=0; i<yres; i++){ */
+  /*   for (j=0; j<xres; j++){ */
+  /*     int n = j % 256; */
+  /*     char c[20]; */
+  /*     sprintf(c, "%d %d %d ", n, n, n); */
+  /*     write(fd, c, strlen(c) * sizeof(char)); */
+  /*   } */
+  /* } */
+
+
   for (i=0; i<yres; i++){
-    for (j=0; j<xres; j++) {
-
-      sprintf(c, "%d ", j);
-      write(fd, c, sizeof(c));
+    for (j=0; j<xres; j++){
+      int n = j % 256;
+      char c[20];
+      sprintf(c, "%d %d %d ", n, (n*n)%256, (n*n*n)%256);
+      write(fd, c, strlen(c) * sizeof(char));
     }
   }
   
-  sprintf(c, "\n");
-  write(fd, c, sizeof(c));
+
   close(fd);
 
 }
